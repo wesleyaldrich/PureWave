f890122e581ddca933a8b4102f767a36fc7faf2a
Wesley Aldrich <169461537+wesleyaldrich@users.noreply.github.com>
2024-12-04 13:51:37 +0700

    Squashed commit of the following:
    commit 9c7aaa8c077cf57d66629269ea9637c69101bcf5
    Author: Wesley Aldrich <169461537+wesleyaldrich@users.noreply.github.com>
    Date:   Wed Dec 4 13:18:18 2024 +0700
    
        Deleted unused comments
    
    commit 4f7a08eb40e545b4f26160fdf0f187284ae8a31a
    Author: Wesley Aldrich <169461537+wesleyaldrich@users.noreply.github.com>
    Date:   Wed Dec 4 13:16:29 2024 +0700
    
        The version used by initial commit of backend
    
    commit 3b105992fecf8c7ecd182288a93ff9b950d28c15
    Author: Wesley Aldrich <169461537+wesleyaldrich@users.noreply.github.com>
    Date:   Wed Dec 4 10:58:58 2024 +0700
    
        Start building
    
    commit 4abb85ce91dc072ffb99ad9b4c45d9942a86c248
    Author: Wesley Aldrich <169461537+wesleyaldrich@users.noreply.github.com>
    Date:   Wed Dec 4 10:01:43 2024 +0700
    
        Remove attempts on backend integration

--- src/Pages/Community.jsx
+++ src/Pages/Community.jsx
@@ -1,35 +1,51 @@
 import './Community.css'
 import Post from '../Components/Post'
+import { useState, useEffect } from 'react';
+import axios from 'axios';
+import { NavLink } from "react-router-dom";
 
 function Community(){
+    const [posts, setPosts] = useState([]);
+
+    const API_BASE_URL = 'http://localhost:8080'
+
+    useEffect(() => {
+        const fetchPosts = async () => {
+            try {
+                const response = await axios.get(`${API_BASE_URL}/data/posts`);
+                setPosts(response.data);
+            } catch (error) {
+                console.error('Error fetching posts:', error);
+          }
+        };
+    
+        fetchPosts();
+    }, []);
+
     return (<>
         <div className="community-page container-fluid flex-col">
             <h1 className="title firacode">COMMUNITY</h1>
             <div className="buttons container-fluid row gurajada">
-                <div className="button col-4">
+                <NavLink to="/lab" className="button col-4" end>
                     <span className="iconHistory">+</span>
-                    <a>ENHANCE YOUR AUDIO</a>
-                </div>
-                <div className="button col">
-                    <a>CREATE POST</a>
-                </div>
+                    <a>ENHANCE YOUR AUDIO</a> 
+                </NavLink>
+                <NavLink to="/create" className="button col" end>
+                    <a>CREATE POST</a> 
+                </NavLink>
             </div>
             <div className="posts container-fluid">
-                <Post
-                    author="Naufal Dimas" 
-                    content="Check out my new rap album, y'all! It's lit af, I rapped with my whole soul!"/>
-                <Post
-                    author="Elak Val" 
-                    content="Lorem ipsum dolor, sit amet consectetur adipisicing elit. Nihil fugiat optio veniam! Dolorum sit maxime enim dolorem similique reiciendis repudiandae, soluta dicta laudantium voluptatibus porro quam voluptas a est quaerat voluptate earum."/>
-                <Post
-                    author="Bos Theda" 
-                    content="Woi ngapain woi?!?!?!"/>
-                <Post
-                    author="Timothy Ronald" 
-                    content="Lorem ipsum dolor, sit amet consectetur adipisicing elit. Alias iste necessitatibus voluptas vel sunt quaerat mollitia tenetur totam libero velit beatae deserunt nesciunt ratione numquam rerum ullam impedit dolores facilis, assumenda asperiores ut, voluptatum non fuga? Itaque vel harum rem et quos a corporis laborum illo totam aspernatur quisquam quaerat placeat deserunt id magnam quo, beatae possimus ipsum ex laboriosam adipisci tempore molestiae sint? Delectus reprehenderit est unde officia laudantium, voluptas commodi similique animi quibusdam suscipit eum dolorem sunt, praesentium impedit! Fugiat obcaecati sapiente laborum reiciendis quas, corrupti facilis placeat vel numquam, aliquam quia repellendus architecto iure ipsam. Nostrum perspiciatis fugit optio sequi, voluptatibus voluptatum repellendus molestias placeat pariatur distinctio veniam praesentium, sunt nam unde, sapiente fugiat et iusto sed!"/>
-                <Post
-                    author="Naufal Dimas" 
-                    content="Diem lu haters!"/>
+                {posts.length > 0 ? (
+                    posts.map(
+                        (post) => (
+                            <Post
+                                picture={post.picture}
+                                author={post.name}
+                                content={post.content} />
+                        )
+                    )
+                ) : (<p>No posts available</p>)
+                }
             </div>
         </div>
     </>)
