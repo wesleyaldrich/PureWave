3b105992fecf8c7ecd182288a93ff9b950d28c15
Wesley Aldrich <169461537+wesleyaldrich@users.noreply.github.com>
2024-12-04 10:58:58 +0700

    Start building

--- src/Pages/Community.jsx
+++ src/Pages/Community.jsx
@@ -1,7 +1,26 @@
 import './Community.css'
 import Post from '../Components/Post'
+import { useState, useEffect } from 'react';
+import axios from 'axios';
 
 function Community(){
+    const [posts, setPosts] = useState([]);
+
+    const API_BASE_URL = 'http://localhost:8080'
+
+    useEffect(() => {
+        const fetchPosts = async () => {
+            try {
+                const response = await axios.get(`${API_BASE_URL}/data/posts`);
+                setPosts(response.data);
+            } catch (error) {
+                console.error('Error fetching posts:', error);
+          }
+        };
+    
+        fetchPosts();
+    }, []);
+
     return (<>
         <div className="community-page container-fluid flex-col">
             <h1 className="title firacode">COMMUNITY</h1>
@@ -15,21 +34,19 @@ function Community(){
                 </div>
             </div>
             <div className="posts container-fluid">
-                <Post
-                    author="Naufal Dimas" 
-                    content="Check out my new rap album, y'all! It's lit af, I rapped with my whole soul!"/>
-                <Post
-                    author="Elak Val" 
-                    content="Lorem ipsum dolor, sit amet consectetur adipisicing elit. Nihil fugiat optio veniam! Dolorum sit maxime enim dolorem similique reiciendis repudiandae, soluta dicta laudantium voluptatibus porro quam voluptas a est quaerat voluptate earum."/>
-                <Post
-                    author="Bos Theda" 
-                    content="Woi ngapain woi?!?!?!"/>
-                <Post
-                    author="Timothy Ronald" 
-                    content="Lorem ipsum dolor, sit amet consectetur adipisicing elit. Alias iste necessitatibus voluptas vel sunt quaerat mollitia tenetur totam libero velit beatae deserunt nesciunt ratione numquam rerum ullam impedit dolores facilis, assumenda asperiores ut, voluptatum non fuga? Itaque vel harum rem et quos a corporis laborum illo totam aspernatur quisquam quaerat placeat deserunt id magnam quo, beatae possimus ipsum ex laboriosam adipisci tempore molestiae sint? Delectus reprehenderit est unde officia laudantium, voluptas commodi similique animi quibusdam suscipit eum dolorem sunt, praesentium impedit! Fugiat obcaecati sapiente laborum reiciendis quas, corrupti facilis placeat vel numquam, aliquam quia repellendus architecto iure ipsam. Nostrum perspiciatis fugit optio sequi, voluptatibus voluptatum repellendus molestias placeat pariatur distinctio veniam praesentium, sunt nam unde, sapiente fugiat et iusto sed!"/>
-                <Post
-                    author="Naufal Dimas" 
-                    content="Diem lu haters!"/>
+                {posts.length > 0 ? (
+                    posts.map(
+                        (post) => (
+                            <Post content={post.content}/>
+                            // <div key={post.id} className="post">
+                            //     <h3>{post.name}</h3>
+                            //     <p>{post.content}</p>
+                            //     <img src={post.picture} alt={`${post.name}'s profile`} />
+                            // </div>
+                        )
+                    )
+                ) : (<p>No posts available</p>)
+                }
             </div>
         </div>
     </>)
