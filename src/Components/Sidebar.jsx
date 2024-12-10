01ceb7425aa55ed9fb2cdbd6ec61f39df2f8f1d1
olivio88 <agustinus.olivio.leoartha@gmail.com>
2024-12-10 10:02:44 +0700

    Update Sidebar

--- src/Components/Sidebar.jsx
+++ src/Components/Sidebar.jsx
@@ -4,6 +4,7 @@ import logo from '../assets/logo.png'
 import icon_community from '../assets/icon-community.png'
 import icon_lab from '../assets/icon-lab.png'
 import icon_history from '../assets/icon-history.png'
+import icon_help from '../assets/icon-help.png'
 
 function Sidebar(){
     return (<>
@@ -13,18 +14,22 @@ function Sidebar(){
                     <img src={logo} alt="PureWave Logo" />
                     <p>PUREWAVE</p>
                 </div>
-
+                
                 <NavLink to="/"  id="community" className="icon center-content" activeClassName="active" end>
-                    <img src={icon_community} alt="PureWave Logo" />
+                    <img src={icon_community} alt="Icon Community" />
                     <p>COMMUNITY</p>
                 </NavLink>
                 <NavLink to="/lab" id="lab" className="icon center-content" activeClassName="active">
-                    <img src={icon_lab} alt="PureWave Logo" />
+                    <img src={icon_lab} alt="Icon Laboratory" />
                     <p>LABORATORY</p>
                 </NavLink>
                 <NavLink to="/history" id="history" className="icon center-content" activeClassName="active">
-                    <img src={icon_history} alt="PureWave Logo" />
+                    <img src={icon_history} alt="Icon History" />
                     <p>HISTORY</p>
+                </NavLink>
+
+                <NavLink to="/" className="help center-content">
+                    <img src={icon_help} alt="Icon Help" />
                 </NavLink>
             </div>
         </div>
