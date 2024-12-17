9bd9dd2beece63d553b2a2e9e409984e5dd4b3cb
danielavall <dvallerine@gmail.com>
2024-12-17 13:36:57 +0700

    Resolve merge conflict on sidebar

--- src/Components/Sidebar.jsx
+++ src/Components/Sidebar.jsx
@@ -5,7 +5,11 @@ import icon_community from '../assets/icon-community.png'
 import icon_lab from '../assets/icon-lab.png'
 import icon_history from '../assets/icon-history.png'
 <<<<<<< HEAD
+<<<<<<< HEAD
 import icon_faq from '../assets/icon-faq.png'
+=======
+import icon_help from '../assets/icon-help.png'
+>>>>>>> c2a9fccca2375b2f223863e0b65b1f01f8df4057
 =======
 import icon_help from '../assets/icon-help.png'
 >>>>>>> c2a9fccca2375b2f223863e0b65b1f01f8df4057
@@ -32,9 +36,14 @@ function Sidebar(){
                     <p>HISTORY</p>
                 </NavLink>
 <<<<<<< HEAD
+<<<<<<< HEAD
                 <NavLink to="/gethelp" className="icon center-content" activeClassName="active">
                     <img src={icon_faq} alt="PureWave Logo" />
                     <p>HELP</p>
+=======
+                <NavLink to="/gethelp" className="help center-content">
+                    <img src={icon_help} alt="Icon Help" />
+>>>>>>> c2a9fccca2375b2f223863e0b65b1f01f8df4057
 =======
                 <NavLink to="/gethelp" className="help center-content">
                     <img src={icon_help} alt="Icon Help" />
