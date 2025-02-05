7fe161da2007476a433acf9e5b1fecdeecfa3f6d
olivio88 <agustinus.olivio.leoartha@gmail.com>
2025-02-05 09:08:05 +0700

    Update Expand Navbar

--- src/Components/Sidebar.jsx
+++ src/Components/Sidebar.jsx
@@ -1,21 +1,35 @@
+import { useState } from "react";
 import { NavLink } from "react-router-dom";
-import './Sidebar.css'
-import logo from '../assets/logo.png'
-import icon_community from '../assets/icon-community.png'
-import icon_lab from '../assets/icon-lab.png'
-import icon_history from '../assets/icon-history.png'
-import icon_help from '../assets/icon-help.png'
+import './Sidebar.css';
+import logo from '../assets/logo.png';
+import doubleArrow from '../assets/icon-doubleArrow.png';
+import icon_community from '../assets/icon-community.png';
+import icon_lab from '../assets/icon-lab.png';
+import icon_history from '../assets/icon-history.png';
+import icon_help from '../assets/icon-help.png';
 
-function Sidebar(){
-    return (<>
-        <div className="sidebar gurajada">
+function Sidebar() {
+    const [isExpanded, setIsExpanded] = useState(false);
+
+    const toggleSidebar = () => {
+        setIsExpanded(!isExpanded);
+    };
+
+    return (
+        <div className={`sidebar ${isExpanded ? "expanded gurajada" : "gurajada"}`}>
+            <div className="ribbonArea" onClick={toggleSidebar}>
+                <div className="ribbon">
+                    <img className="arrow" src={doubleArrow} alt="Arrow Icon" />
+                </div>
+            </div>
+
             <div className="sidebar-content flex-col">
                 <div className="logo center-content">
                     <img src={logo} alt="PureWave Logo" />
                     <p>PUREWAVE</p>
                 </div>
-                
-                <NavLink to="/"  id="community" className="icon center-content" activeClassName="active" end>
+
+                <NavLink to="/" id="community" className="icon center-content" activeClassName="active" end>
                     <img src={icon_community} alt="Icon Community" />
                     <p>COMMUNITY</p>
                 </NavLink>
@@ -32,7 +46,7 @@ function Sidebar(){
                 </NavLink>
             </div>
         </div>
-    </>)
+    );
 }
 
-export default Sidebar
+export default Sidebar;
