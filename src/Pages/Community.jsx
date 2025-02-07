50b4e4393dcdcf679047c5b25a4823b6f6a9799e
Wesley Aldrich <169461537+wesleyaldrich@users.noreply.github.com>
2025-02-07 10:57:53 +0700

    Fix small bug in staticPost declaration

--- src/Pages/Community.jsx
+++ src/Pages/Community.jsx
@@ -307,7 +307,7 @@ function Community({ isSidebarExpanded }) {
         }
     }, [isReplyOpen]);
 
-    staticPost = {
+    const staticPost = {
         picture: dummy_pic,
         author: "Dummy Guy",
         content: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
